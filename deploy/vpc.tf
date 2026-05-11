bcead32d42cb1f7946ea5223d99581dea5f996a6
Diego Rodriguez <alejo_1421@hotmail.com>
2026-05-11 15:17:57 +0200

    [DR-0003] Add RT associations

--- deploy/vpc.tf
+++ deploy/vpc.tf
@@ -8,15 +8,28 @@ resource "aws_subnet" "cmtr-66o53piv-01-subnet-public-a" {
   availability_zone = "us-east-1a"
   cidr_block        = var.subnets_config["az-a"]
 }
+resource "aws_route_table_association" "rta-az-a" {
+  subnet_id      = aws_subnet.cmtr-66o53piv-01-subnet-public-a.id
+  route_table_id = aws_route_table.cmtr-66o53piv-01-rt.id
+}
 
 resource "aws_subnet" "cmtr-66o53piv-01-subnet-public-b" {
   vpc_id            = aws_vpc.cmtr-66o53piv-01-vpc.id
   availability_zone = "us-east-1b"
   cidr_block        = var.subnets_config["az-b"]
 }
+resource "aws_route_table_association" "rta-az-b" {
+  subnet_id      = aws_subnet.cmtr-66o53piv-01-subnet-public-b.id
+  route_table_id = aws_route_table.cmtr-66o53piv-01-rt.id
+}
 
 resource "aws_subnet" "cmtr-66o53piv-01-subnet-public-c" {
   vpc_id            = aws_vpc.cmtr-66o53piv-01-vpc.id
   availability_zone = "us-east-1c"
   cidr_block        = var.subnets_config["az-c"]
+}
+
+resource "aws_route_table_association" "rta-az-c" {
+  subnet_id      = aws_subnet.cmtr-66o53piv-01-subnet-public-c.id
+  route_table_id = aws_route_table.cmtr-66o53piv-01-rt.id
 }
