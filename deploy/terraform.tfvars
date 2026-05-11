cf825a71cd2b2f536d8ad3f09f103abf40c54a5b
Diego Rodriguez <alejo_1421@hotmail.com>
2026-05-11 16:12:12 +0200

    [DR-0005] Remove Defaults

--- deploy/terraform.tfvars
+++ deploy/terraform.tfvars
@@ -1,2 +1,7 @@
 vpc_cidr   = "10.10.0.0/16"
 aws_region = "us-east-1"
+subnets_config = {
+  az-a = "10.10.1.0/24"
+  az-b = "10.10.3.0/24"
+  az-c = "10.10.5.0/24"
+}
--- deploy/variables.tf
+++ deploy/variables.tf
@@ -1,21 +1,14 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
   description = "VPC CIDR Block"
   type        = string
-  default     = "10.10.0.0/16"
 }
 
 variable "subnets_config" {
   type        = map(string)
   description = "VPC AZ Subnets CIDR Blocks"
-  default = {
-    az-a = "10.10.1.0/24"
-    az-b = "10.10.3.0/24"
-    az-c = "10.10.5.0/24"
-  }
 }
